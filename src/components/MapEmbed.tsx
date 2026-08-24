3a4fdd3544215d58611e4087bdbb916ad59e685c
hadar hubara <hadarhubara10@gmail.com>
2026-08-24 16:09:12 +0300

    fix: update MapEmbed iframe source URL to the new format

--- src/components/MapEmbed.tsx
+++ src/components/MapEmbed.tsx
@@ -4,7 +4,7 @@ export const MapEmbed: React.FC = () => {
   return (
     <div className="w-full">
       <iframe
-        src="https://www.google.com/maps/d/embed?mid=1bsZLIyGgobq7Lm6FDt3q3IzsoWTbvyc&ehbc=2E312F"
+        src="https://www.google.com/maps/d/u/0/embed?mid=1yq1ias0117bGZhv_s6QMoX7kS8aqyAo&ehbc=2E312F"
         className="w-full h-96 border-0 rounded-lg"
         title="מפת נקודות סכנה"
         loading="lazy"
